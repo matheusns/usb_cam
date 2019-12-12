c7d2232fbf58f0d8d46a4c5b000384de20a82f1b
Matheus Nascimento <contamatheusnascimento@gmail.com>
2019-12-11 21:48:43 -0300

    Add initial structure

---
+++ src/usb_cam_pub.cpp
@@ -0,0 +1,69 @@
+//  BSD 3-Clause License
+//  Copyright (c) 2019, Matheus Nascimento, Luciana Reys
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//  1. Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//  3. Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  Students: Matheus do Nascimento Santos 1920858  (@matheusns)
+//            Luciana Reys 1920856 (@lsnreys)
+//  Professor: Wouter Caarls (@wcaarls)
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sstream> // for converting the command line parameter to integer
+
+int main(int argc, char** argv)
+{
+  // Check if video source has been passed as a parameter
+  if(argv[1] == NULL) return 1;
+
+  ros::init(argc, argv, "image_publisher");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+  // Convert the passed as command line parameter index for the video device to an integer
+  std::istringstream video_sourceCmd(argv[1]);
+  int video_source;
+  // Check if it is indeed a number
+  if(!(video_sourceCmd >> video_source)) return 1;
+
+  cv::VideoCapture cap(video_source);
+  // Check if video device can be opened with the given index
+  if(!cap.isOpened()) return 1;
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+
+  ros::Rate loop_rate(24);
+  while (nh.ok()) {
+    cap >> frame;
+    // Check if grabbed frame is actually full with some content
+    if(!frame.empty()) {
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      pub.publish(msg);
+      cv::waitKey(1);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
